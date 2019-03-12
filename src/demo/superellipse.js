636bbe91ee474abb7d0bd94fb2d7daf76519f0e1
Torbjørn Mathisen <herrmathisen@gmail.com>
2019-03-12 19:50:55 +0100

    Add superellipse demo

---
+++ src/demo/superellipse.js
@@ -0,0 +1,25 @@
+import Tarpaulin from "../"
+
+const n = 5, a = 60, b = 60
+
+const size = 400
+const xMin = -a
+const xMax = a
+const yMin = -b
+const yMax = b
+
+// Create Canvas
+
+Tarpaulin.create({ size, xMin, xMax, yMin, yMax })
+
+// Start drawing
+
+const points = []
+
+for (let t = 0; t < 2 * Math.PI; t += .1) {
+    const x = Math.pow(Math.abs(Math.cos(t)), 2 / n) * a * Math.sign(Math.cos(t))
+    const y = Math.pow(Math.abs(Math.sin(t)), 2 / n) * b * Math.sign(Math.sin(t))
+    points.push([x, y])
+}
+
+Tarpaulin.drawPath(points, { fillStyle: Tarpaulin.Color.Blue })
