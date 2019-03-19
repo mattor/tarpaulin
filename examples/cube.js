20b757a99b78bdfbfee7592fdb17ad4677e5d226
Torbjørn Mathisen <herrmathisen@gmail.com>
2019-03-19 21:21:27 +0100

    Add rotating cube example

---
+++ examples/cube.js
@@ -0,0 +1,82 @@
+import Tarpaulin from "../src"
+const { Point3D, Color } = Tarpaulin
+
+// Set appearance
+const size = 300
+const xMin = -1
+const xMax = 1
+const yMin = -1
+const yMax = 1
+
+// Create Canvas
+Tarpaulin.create({ size, xMin, xMax, yMin, yMax })
+
+const vertices = [
+    new Point3D(-1, 1, -1),
+    new Point3D(1, 1, -1),
+    new Point3D(1, -1, -1),
+    new Point3D(-1, -1, -1),
+    new Point3D(-1, 1, 1),
+    new Point3D(1, 1, 1),
+    new Point3D(1, -1, 1),
+    new Point3D(-1, -1, 1),
+]
+
+// The vertices for each of the 6 faces, indices to defined vertices
+const faces = [
+    [0, 1, 2, 3],
+    [1, 5, 6, 2],
+    [5, 4, 7, 6],
+    [4, 0, 3, 7],
+    [0, 4, 5, 1],
+    [3, 2, 6, 7],
+]
+
+const colors = [
+    Color.Red,
+    Color.Green,
+    Color.Blue,
+    Color.Yellow,
+    Color.Cyan,
+    Color.Purple,
+]
+
+let angle = 0
+
+Tarpaulin.animate(() => {
+    const projectedVerticies = vertices.map(vertex => {
+        const rotatedVertex = vertex
+            .rotateX(angle)
+            .rotateY(angle)
+        return rotatedVertex.project(5, 10)
+    })
+
+    const sortedFaces = faces
+        .map((face, index) => ({
+            index,
+            z: (
+                projectedVerticies[face[0]].z
+                + projectedVerticies[face[1]].z
+                + projectedVerticies[face[2]].z
+                + projectedVerticies[face[3]].z
+            ) / face.length,
+        }))
+        .sort((a, b) => b.z - a.z)
+
+    // Draw
+
+    Tarpaulin.clearCanvas({ fillStyle: Color.Black })
+
+    sortedFaces.forEach(sortedFace => {
+        const face = faces[sortedFace.index]
+
+        Tarpaulin.drawPath([
+            [projectedVerticies[face[0]].x, projectedVerticies[face[0]].y],
+            [projectedVerticies[face[1]].x, projectedVerticies[face[1]].y],
+            [projectedVerticies[face[2]].x, projectedVerticies[face[2]].y],
+            [projectedVerticies[face[3]].x, projectedVerticies[face[3]].y],
+        ], { fillStyle: colors[sortedFace.index] })
+    })
+
+    angle += 1
+})
