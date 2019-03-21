1db794d4be78d8e6006fc25dcc4ed55c35ee37e6
Torbjørn Mathisen <herrmathisen@gmail.com>
2019-03-21 19:31:41 +0100

    Add Const for derived constants

---
+++ src/const/Const.js
@@ -0,0 +1,7 @@
+import { convertDegToRad } from "../helpers"
+
+export const RADIANS_30_DEGREES = convertDegToRad(30)
+export const RADIANS_45_DEGREES = convertDegToRad(45)
+export const RADIANS_60_DEGREES = convertDegToRad(60)
+export const RADIANS_90_DEGREES = convertDegToRad(90)
+export const RADIANS_360_DEGREES = convertDegToRad(360)
--- src/const/index.js
+++ src/const/index.js
@@ -1,5 +1,7 @@
 import * as Color from "./Color"
+import * as Const from "./Const"
 
 export {
     Color,
+    Const,
 }
