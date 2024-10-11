90c1c017c66ed0c99c9dc874ab8f277ca6261908
Torbjørn Mathisen <torbjorn.mathisen@trumf.no>
2024-10-11 13:10:48 +0200

    Examples: Switch to vite

---
+++ examples/vite.config.mts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import fs from 'fs'
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    const example = process.env.example || "hilbert"
+    const examplePath = `./src/${example}.js`
+
+    if (!fs.existsSync(examplePath)) {
+        throw new Error(`No example called "${process.env.example}"`)
+    }
+
+    console.log(`Building example: ${example}`)
+
+    return {
+        resolve: {
+            alias: {
+                "tarpaulin": resolve(__dirname, "../src/index.js"),
+                "example": resolve(__dirname, examplePath),
+            },
+        }
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
-const Tarpaulin = require("./Tarpaulin")
+import {
+    Color,
+    Const,
+} from "./const"
+import {
+    drawAxes,
+    drawCircle,
+    drawGrid,
+    drawLine,
+    drawPath,
+    drawPixel,
+    drawRect,
+} from "./draw"
+import {
+    ComplexNumber,
+    Point3D,
+} from "./entities"
+import {
+    getMinMax,
+    getNextPoint,
+    getXYCoords,
+} from "./helpers"
+import {
+    animate,
+    animatePath,
+    clear,
+    createCanvas,
+    createSvg,
+} from "./utils"
 
-module.exports = Tarpaulin.default
+const Tarpaulin = {
+    // const
+    Color,
+    Const,
+    // entities
+    ComplexNumber,
+    Point3D,
+    // utils
+    animate,
+    animatePath,
+    clear,
+    createCanvas,
+    createSvg,
+    // draw
+    drawAxes,
+    drawCircle,
+    drawGrid,
+    drawLine,
+    drawPath,
+    drawPixel,
+    drawRect,
+    // helpers
+    getMinMax,
+    getNextPoint,
+    getXYCoords,
+}
+
+export default Tarpaulin
+
+export {
+    // const
+    Color,
+    Const,
+    // entities
+    ComplexNumber,
+    Point3D,
+    // utils
+    animate,
+    animatePath,
+    clear,
+    createCanvas,
+    createSvg,
+    // draw
+    drawAxes,
+    drawCircle,
+    drawGrid,
+    drawLine,
+    drawPath,
+    drawPixel,
+    drawRect,
+    // helpers
+    getMinMax,
+    getNextPoint,
+    getXYCoords,
+}
