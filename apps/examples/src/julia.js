5f13a70790ba6d484960a2a2f546e9337b48ced8
Torbjørn Mathisen <torbjorn.mathisen@noaignite.com>
2025-09-09 15:06:38 +0200

    Make julia example interactive

--- apps/examples/src/julia.js
+++ apps/examples/src/julia.js
@@ -6,11 +6,9 @@ const xMin = -2
 const xMax = 2
 const yMin = -4 / 3
 const yMax = 4 / 3
-const pixelRatio = 2
+const pixelRatio = window.devicePixelRatio
 
-const c = [-1, 1 / 4] // all complex number are in the form of [x, y] which means x + i*y
-const maxIterate = 50
-const R = (1 + Math.sqrt(1 + 4 * abs(c))) / 2
+const maxIterations = 50
 
 function f(z, c) { // calculate the value of the function with complex arguments.
     return [z[0] * z[0] - z[1] * z[1] + c[0], 2 * z[0] * z[1] + c[1]]
@@ -20,13 +18,12 @@ function abs(z) { // absolute value of a complex number
     return Math.sqrt(z[0] * z[0] + z[1] * z[1])
 }
 
-function checkIfBelongsToJuliaSet([x, y]) {
-    let z = [x, y]
-    for (let i = 0; i < maxIterate; i++) {
+function isInJuliaSet(z, c, R) {
+    for (let i = 0; i < maxIterations; i++) {
         z = f(z, c)
         if (abs(z) > R) {
             // Return a number as a percentage
-            return i / maxIterate * 100
+            return i / maxIterations * 100
         }
     }
     // Return zero if in set
@@ -35,18 +32,32 @@ function checkIfBelongsToJuliaSet([x, y]) {
 
 // Create tarp
 
-const { tarp, tarpWidth, tarpHeight } = T.createCanvas({ size, xMin, xMax, yMin, yMax, pixelRatio })
+const { tarpElement, tarpWidth, tarpHeight } = T.createCanvas({ size, xMin, xMax, yMin, yMax, pixelRatio })
 
-// Start drawing
+function draw(c) {
+    const R = (1 + Math.sqrt(1 + 4 * abs(c))) / 2
 
-for (let x = 0; x < tarpWidth; x++) {
-    for (let y = 0; y < tarpHeight; y++) {
-        const belongsToSet = checkIfBelongsToJuliaSet(T.getXYCoords([x, y]))
-        if (belongsToSet === 0) {
-            T.drawPixel([x, y], { fill: T.Color.Black })
-        }
-        else {
-            T.drawPixel([x, y], { fill: `hsl(0, 100%, ${belongsToSet}%)` })
+    for (let x = 0; x < tarpWidth; x++) {
+        for (let y = 0; y < tarpHeight; y++) {
+            const belongsToSet = isInJuliaSet(T.getXYCoords([x, y]), c, R)
+            if (belongsToSet === 0) {
+                T.drawPixel([x, y], { fill: T.Color.Black })
+            }
+            else {
+                T.drawPixel([x, y], { fill: `hsl(0, 100%, ${belongsToSet}%)` })
+            }
         }
     }
 }
+
+// Start drawing
+
+draw([-1, 1 / 4]) // all complex number are in the form of [a, b] which means a + i*b
+
+tarpElement.addEventListener("mousemove", (e) => {
+    const localX = e.offsetX
+    const localY = e.offsetY
+    const a = T.remapValueBetweenScales(localX, 0, e.target.width / pixelRatio, xMin, xMax)
+    const b = T.remapValueBetweenScales(localY, 0, e.target.height / pixelRatio, yMin, yMax)
+    draw([a, b])
+})
